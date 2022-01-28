3d497adcbf4311f7e23fafdc28847ec600a69593
Moritz Gnisia <moritzgnisia@gmail.com>
2022-01-28 22:03:04 +0100

    Adding poetry and starting to remove strip dependency

--- order/views.py
+++ order/views.py
@@ -1,4 +1,3 @@
-import stripe
 
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -13,24 +12,17 @@ from rest_framework.response import Response
 from .models import Order, OrderItem
 from .serializers import OrderSerializer, MyOrderSerializer
 
+# @authentication_classes([authentication.TokenAuthentication])
+# @permission_classes([permissions.IsAuthenticated])
 @api_view(['POST'])
-@authentication_classes([authentication.TokenAuthentication])
-@permission_classes([permissions.IsAuthenticated])
 def checkout(request):
     serializer = OrderSerializer(data=request.data)
 
     if serializer.is_valid():
-        stripe.api_key = settings.STRIPE_SECRET_KEY
+        # stripe.api_key = settings.STRIPE_SECRET_KEY
         paid_amount = sum(item.get('quantity') * item.get('product').price for item in serializer.validated_data['items'])
 
         try:
-            charge = stripe.Charge.create(
-                amount=int(paid_amount * 100),
-                currency='USD',
-                description='Charge from Djackets',
-                source=serializer.validated_data['stripe_token']
-            )
-
             serializer.save(user=request.user, paid_amount=paid_amount)
 
             return Response(serializer.data, status=status.HTTP_201_CREATED)
